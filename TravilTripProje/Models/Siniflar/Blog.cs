40c2bfe6a99ef35c5b119a42c1656dab6db32df1
Huseyin ARKIN <99.arkn@gmail.com>
2021-10-11 12:43:51 +0300

    Modeller Oluşturuldu.

---
+++ TravilTripProje/Models/Siniflar/Blog.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace TravilTripProje.Models.Siniflar
+{
+    public class Blog
+    {
+        [Key]
+        public int ID { get; set; }
+        public string Baslik { get; set; }
+        public DateTime Aciklama { get; set; }
+        public string BlogImage { get; set; }
+    }
+}
