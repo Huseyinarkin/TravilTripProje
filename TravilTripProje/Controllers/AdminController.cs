9e83783737d495378f8d4a3e640fa8550e165b70
Huseyin ARKIN <99.arkn@gmail.com>
2021-10-12 17:09:00 +0300

    Admin paneli yeni blog ekleme.

--- TravilTripProje/Controllers/AdminController.cs
+++ TravilTripProje/Controllers/AdminController.cs
@@ -16,5 +16,17 @@ namespace TravilTripProje.Controllers
             var degerler = c.Blogs.ToList();
             return View(degerler);
         }
+        [HttpGet]
+        public ActionResult YeniBlog()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult YeniBlog(Blog p)
+        {
+            c.Blogs.Add(p);
+            c.SaveChanges();
+            return RedirectToAction("Index");
+        }
     }
 }
