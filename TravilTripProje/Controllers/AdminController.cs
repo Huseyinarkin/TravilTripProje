d9b893373a6845c0449bdf211091567f9b11ef7f
Huseyin ARKIN <99.arkn@gmail.com>
2021-10-12 19:02:32 +0300

    Admiin panel yorum listele sil güncelle işlemleri eklendi.

--- TravilTripProje/Controllers/AdminController.cs
+++ TravilTripProje/Controllers/AdminController.cs
@@ -50,5 +50,31 @@ namespace TravilTripProje.Controllers
             c.SaveChanges();
             return RedirectToAction("Index");
         }
+        public ActionResult YorumListele()
+        {
+            var yorumlar = c.Yorumlars.ToList();
+            return View(yorumlar);
+        }
+        public ActionResult YorumSil(int id)
+        {
+            var b = c.Yorumlars.Find(id);
+            c.Yorumlars.Remove(b);
+            c.SaveChanges();
+            return RedirectToAction("YorumListele");
+        }
+        public ActionResult YorumGetir(int id)
+        {
+            var yr = c.Yorumlars.Find(id);
+            return View("YorumGetir",yr);
+        }
+        public ActionResult YorumGuncelle(Yorumlar y)
+        {
+            var yrm = c.Yorumlars.Find(y.ID);
+            yrm.KullaniciAdi = y.KullaniciAdi;
+            yrm.Mail = y.Mail;
+            yrm.Yorum = y.Yorum;            
+            c.SaveChanges();
+            return RedirectToAction("YorumListele");
+        }
     }
 }
